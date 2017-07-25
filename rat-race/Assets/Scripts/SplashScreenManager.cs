64082b5bc48dccb10d178238fdc44db821ce8a1e
Andrew Lott <AndrewLott@gmail.com>
2017-07-24 18:44:53 -0700

    adding splash screen difficulty settings and high score display"

--- rat-race/Assets/Scripts/SplashScreenManager.cs
+++ rat-race/Assets/Scripts/SplashScreenManager.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class SplashScreenManager : MonoBehaviour {
+	public Slider slider;
+	public Text highScoreText;
+	public Text difficultyText;
 
 	void Start () {
+		int highScore = PlayerPrefs.GetInt("highScore");
+		slider.value = PlayerPrefs.GetInt("difficulty") != 0 ? (int)PlayerPrefs.GetInt("difficulty") : 1;
+		if (highScore > 0) {
+			this.highScoreText.text = string.Format("Highest Level: {0}", highScore);
+			slider.maxValue = (float)highScore;
+		} else {
+			this.highScoreText.text = "";
+		}
+		this.UpdateDifficultyText();
 	}
 
 	void Update () {
+	}
+
+	private void UpdateDifficultyText() {
+		int sliderValue = (int)this.slider.value;
+		this.difficultyText.text = string.Format("Difficulty: {0}", sliderValue);
+	}
+
+	public void OnDifficultySliderUpdated() {
+		int sliderValue = (int)this.slider.value;
+		PlayerPrefs.SetInt("difficulty", sliderValue);
+		this.UpdateDifficultyText();
 	}
 
 	public void LoadGame() {
